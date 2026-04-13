08e20aa0e840c09a38f8b490b3b54759a51bf6ce
mak-1007 <mohammadamankhan8900@gmail.com>
2026-04-13 21:18:52 +0530

    Add more APIs

--- HospitalManagementSystem/src/main/java/com/example/HospitalManagementSystem/PatientController.java
+++ HospitalManagementSystem/src/main/java/com/example/HospitalManagementSystem/PatientController.java
@@ -1,5 +1,6 @@
 package com.example.HospitalManagementSystem;
 
+import ch.qos.logback.core.read.ListAppender;
 import org.springframework.web.bind.annotation.*;
 import tools.jackson.databind.deser.CreatorProperty;
 
@@ -11,12 +12,14 @@ import java.util.List;
 public class PatientController {
     HashMap<Integer, Patient> patientObj =new HashMap<>();
 
+
     @PostMapping("/addPatientViaRequestParameter")
     public String addPatient(@RequestParam("patientId")Integer patientId,@RequestParam("name")String name, @RequestParam("disease")String disease,@RequestParam("age")Integer age){
         Patient patient=new Patient(patientId,name,disease,age);
         patientObj.put(patientId,patient);
         return "patient addded sucessfully";
     }
+
     @PostMapping("/addPatientViaRequestBody")
     public String addPatient(@RequestBody Patient patient){
             int key =patient.getPatientId();
@@ -24,6 +27,8 @@ public class PatientController {
 
         return "patient added successfully";
     }
+
+
 
     @GetMapping("/getPatientInfo")
     public Patient getPatientInformation(@RequestParam("patientId") Integer patientId) {
@@ -37,6 +42,7 @@ public class PatientController {
 //    }
 
 
+
     @GetMapping("/getPatientByName")
     public Patient getPatientByName(@RequestParam("name")String name) {
 
@@ -48,10 +54,12 @@ public class PatientController {
         return null;
     }
 
+
     @GetMapping("/getAllPatients")
     public List<Patient> getAllPatients(){
         return new ArrayList<>(patientObj.values());
     }
+
 
     @GetMapping ("/getPatientsListGreaterThanAge")
     public  List<Patient> getPatientsListGreaterThanAge(@RequestParam("age")Integer age){
@@ -62,5 +70,55 @@ public class PatientController {
             }
         }
         return patients;
+    }
+
+
+    @GetMapping("/getInfoViaPathVar/{patientId}")
+    public Patient getInfoViaPathVar(@PathVariable("patientId")Integer patientId){
+
+        Patient patient = patientObj.get(patientId);
+        return patient;
+    }
+
+
+
+
+
+
+    @GetMapping("/getInfoViaMultiplePathVar/{age}/{disease}")
+    public  List<Patient> getInfoViaMultiplePathVar(@PathVariable("age")Integer age,@PathVariable("disease")String disease){
+        List <Patient> patients=new ArrayList<>();
+        for(Patient p: patientObj.values()){
+            if (p.getAge()>age && p.getDisease().equals(disease)){
+                patients.add(p);
+            }
+        }
+        return patients;
+    }
+
+
+    @PutMapping("/updatePatientInfo")
+    public String updatePatientInfo(@RequestBody Patient patient){
+        int key = patient.getPatientId();
+        if (patientObj.containsKey(key)){
+            patientObj.put(key,patient);
+            return "Info updated successfully";
+
+        }
+        else {
+            return "data is not existing";
+        }
+    }
+
+    @PutMapping("updateInfoViaRequesParam")
+    public String updateInfoViaRequesParam(@RequestParam("patientId")Integer patientId,@RequestParam("disease")String disease){
+        if (patientObj.containsKey(patientId)){
+            Patient patient=patientObj.get(patientId);
+            patient.setDisease(disease);
+            patientObj.put(patientId,patient);
+            return "Info updated successfully";
+        }else {
+            return "data is not existing";
+        }
     }
 }
